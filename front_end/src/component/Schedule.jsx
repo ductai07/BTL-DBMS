9721eebe90a5b84ad53d16370e291108149880cc
pddabcptitu <phamdinh11012004@gmail.com>
2025-05-04 12:25:36 +0700

    upd showtime

--- front_end/src/component/Schedule.jsx
+++ front_end/src/component/Schedule.jsx
@@ -1,56 +1,14 @@
-import TextField from '@mui/material/TextField';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { vi } from 'date-fns/locale';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { IconButton, InputAdornment } from '@mui/material';
-import ClearIcon from '@mui/icons-material/Clear';
-import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
-
-export default function Schedule() {
-  const [value, setValue] = React.useState(null);
-  const [error, setError] = React.useState(false);
-
-  const handleDateChange = (newValue) => {
-    setValue(newValue);
-    setError(false);
-  };
-
+const Schedule = ({ date, setDate }) => {
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={vi}>
-      <DatePicker
-        label="Chọn ngày"
-        value={value}
-        onChange={handleDateChange}
-        inputFormat="dd/MM/yyyy"
-        componentsProps={{
-          actionBar: {
-            actions: ['clear', 'today'],
-          },
-        }}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            error={error}
-            helperText={error ? 'Vui lòng chọn ngày hợp lệ' : ''}
-            InputProps={{
-              ...params.InputProps,
-              endAdornment: (
-                <>
-                  {value && (
-                    <InputAdornment position="end">
-                      <IconButton onClick={() => setValue(null)}>
-                        <ClearIcon />
-                      </IconButton>
-                    </InputAdornment>
-                  )}
-                  {params.InputProps?.endAdornment}
-                </>
-              ),
-            }}
-          />
-        )}
+    <div>
+      <input
+        className="h-7 w-44 rounded-md border border-solid border-gray-400 text-sm font-medium px-2 hover:cursor-pointer"
+        type="date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
       />
-    </LocalizationProvider>
+    </div>
   );
-}
+};
+
+export default Schedule;
--- front_end/src/pages/Showtime.jsx
+++ front_end/src/pages/Showtime.jsx
@@ -3,9 +3,9 @@ import Header from "../component/Header";
 import Search from "../component/Search";
 import Select from "../component/Select";
 import Schedule from "../component/Schedule";
+import { FaPlus } from "react-icons/fa";
 
 const Showtime = () => {
-  // { key: "all", value: "All Room Types" },
   const roomOptions = [
     { key: "all", value: "--Tất cả phòng--" },
     { key: "room1", value: "Phòng 1" },
@@ -23,35 +23,72 @@ const Showtime = () => {
     { key: "shown", value: "Đang chiếu" },
     { key: "showned", value: "Đã chiếu" },
   ];
+
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const handleReset = () => {
+    setEndDate("");
+    setStartDate("");
+    setShowTimeRoom(roomOptions[0].value);
+  };
+
   return (
     <div className="w-[100%] h-[100vh]  bg-neutral-100  p-5 overflow-auto">
       <Header title={"Showtime"} />
-      <div>
-        <div>
-          <div>Tên phim</div>
-          <Search placeholder={"Nhập tên phim"} />
+      <div className="mb-6  bg-white p-5 shadow-md rounded-xl">
+        <div className="flex gap-2 pb-4 justify-between">
+          <div className="flex gap-2">
+            <div>
+              <div>Tên phim</div>
+              <Search placeholder={"Nhập tên phim"} />
+            </div>
+            <div>
+              <div>Phòng chiếu</div>
+              <Select
+                options={roomOptions}
+                defaultValue={showTimeRoom}
+                setDefault={setShowTimeRoom}
+                keyStorage={"showTimeRoom"}
+              />
+            </div>
+            <div>
+              <div>Trạng thái</div>
+              <Select
+                options={statusOptions}
+                defaultValue={showTimeRoom}
+                setDefault={setShowTimeRoom}
+                keyStorage={"showTimeRoom"}
+              />
+            </div>
+          </div>
+          <button className="button flex items-center gap-1">
+            <FaPlus />
+            Showtime
+          </button>
         </div>
-        <div>
-          <div>Phòng chiếu</div>
-          <Select
-            options={roomOptions}
-            defaultValue={showTimeRoom}
-            setDefault={setShowTimeRoom}
-            keyStorage={"showTimeRoom"}
-          />
+        <div className="flex gap-2 pb-4">
+          <div>
+            <span>Từ ngày</span>
+            <Schedule date={startDate} setDate={setStartDate} />
+          </div>
+          <div>
+            <span>Đến ngày</span>
+            <Schedule date={endDate} setDate={setEndDate} />
+          </div>
         </div>
-        <div>
-          <div>Trạng thái</div>
-          <Select
-            options={statusOptions}
-            defaultValue={showTimeRoom}
-            setDefault={setShowTimeRoom}
-            keyStorage={"showTimeRoom"}
-          />
-        </div>
-        <div>
-          <div>Từ nay</div>
-          <Schedule />
+        <div className="flex gap-2 pb-4">
+          <div
+            className="button !bg-white !text-black border border-black flex items-center justify-center hover:cursor-pointer"
+            onClick={handleReset}
+          >
+            Đặt lại
+          </div>
+          <div
+            className="button flex items-center justify-center hover:cursor-pointer"
+            handleSearch
+          >
+            Tìm kiếm
+          </div>
         </div>
       </div>
     </div>
